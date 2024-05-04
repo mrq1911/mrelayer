9811315c0ca4df0451b3eb525e68279a613710a1
mrq <mrq@mrq.cz>
2024-05-04 20:25:49 +0200

    completing transfers

--- src/app.ts
+++ src/app.ts
@@ -4,71 +4,71 @@ import {
   StandardRelayerContext,
 } from "@wormhole-foundation/relayer-engine";
 import {CHAIN_ID_ACALA, CHAIN_ID_ETH, CHAIN_ID_MOONBEAM, ChainId, TokenBridgePayload} from "@certusone/wormhole-sdk";
-import * as winston from "winston";
+import logger from "./logger";
+import {Contract, ethers} from "ethers";
+import {VersionedUserAction} from "./gmp";
 
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      level: process.env.LOG_LEVEL || "info"
-    }),
-  ],
-  format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.splat(),
-      winston.format.simple(),
-      winston.format.timestamp({
-        format: "YYYY-MM-DD HH:mm:ss.SSS",
-      }),
-      winston.format.errors({ stack: true }),
-  ),
-});
-
+const MRL_ADDRESS = "0000000000000000000000000000000000000000000000000000000000000816";
+const GMP_ABI = ['function wormholeTransferERC20(bytes) external',];
+const moonbeam = new ethers.providers.JsonRpcProvider('https://moonbeam-rpc.dwellir.com');
+const signer = new ethers.Wallet(process.env.PRIVKEY, moonbeam);
+const gmp = new Contract('0x0000000000000000000000000000000000000816', GMP_ABI, signer);
 
 (async function main() {
-  // initialize relayer engine app, pass relevant config options
+  let currentNonce = await moonbeam.getTransactionCount(signer.address);
+  const nextNonce = () => currentNonce++
+  logger.info(`account ${signer.address}`);
+  logger.info(`nonce ${currentNonce}`);
+
   const app = new StandardRelayerApp<StandardRelayerContext>(
     Environment.MAINNET,
-    // other app specific config options can be set here for things
-    // like retries, logger, or redis connection settings.
     {
-      name: `mrelayer4`,
+      name: `mrelayer11`,
       logger,
       missedVaaOptions: {
         startingSequenceConfig: {
           [CHAIN_ID_ACALA as ChainId]: BigInt(2600),
-          [CHAIN_ID_ETH as ChainId]: BigInt(268000),
+          [CHAIN_ID_ETH as ChainId]: BigInt(269379),
         }
       }
     },
   );
 
-  const MRL_ADDRESS = "0000000000000000000000000000000000000000000000000000000000000816";
-
-  // add a filter with a callback that will be
-  // invoked on finding a VAA that matches the filter
   app.tokenBridge([CHAIN_ID_ACALA, CHAIN_ID_ETH],
     async (ctx, next) => {
       const { payload } = ctx.tokenBridge;
-      const { vaa } = ctx;
-      const {payloadType, toChain} = payload;
+      const { vaa, sourceTxHash } = ctx;
+      const {payloadType, toChain, tokenTransferPayload} = payload;
       const to = payload.to.toString("hex");
+      const logger = ctx.logger.child({sourceTxHash});
 
       if (toChain === CHAIN_ID_MOONBEAM
           && payloadType === TokenBridgePayload.TransferWithPayload
           && to === MRL_ADDRESS) {
-        ctx.logger.info(
-            `Transfer processing for: \n` +
-            `\tToken: ${payload.tokenChain}:${payload.tokenAddress.toString(
-                "hex",
-            )}\n` +
-            `\tAmount: ${payload.amount}\n` +
-            `\tSender ${payload.fromAddress?.toString("hex")}\n` +
-            `\tReceiver: ${payload.toChain}:${payload.to.toString("hex")}\n` +
-            `\tPayload: ${payload.tokenTransferPayload.toString("hex")}\n`,
-        );
-      }
 
-      next();
+        // TODO parse payload to filter only hydra ones
+        //new VersionedUserAction('0x' + tokenTransferPayload.toString("hex"))
+
+        try {
+          logger.info(`Found VAA`);
+          await gmp.callStatic.wormholeTransferERC20(vaa.bytes, {nonce: currentNonce})
+          logger.info(`Completing transfer`);
+          const tx = await gmp.wormholeTransferERC20(vaa.bytes, {nonce: nextNonce()});
+          logger.info(`Transfer completed: ${tx.hash}`);
+          return next();
+        } catch (e) {
+          const text = JSON.stringify(e);
+          if (text.indexOf('transfer already completed') !== -1) {
+            logger.info(`Transfer already completed`);
+            return next();
+          }
+          if (text.indexOf('Invalid GMP Payload') !== -1) {
+            logger.info(`Invalid GMP payload`);
+            return next();
+          }
+          ctx.logger.error(e.error || e.message || e);
+        }
+      }
     },
   );
 

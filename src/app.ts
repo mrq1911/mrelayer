d666d112296f33ee96813079feae21f1e16a016e
mrq <mrq@mrq.cz>
2025-04-03 13:30:41 +0200

    nonce handling fix

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,14 @@ import {
   StandardRelayerApp,
   StandardRelayerContext,
 } from "@wormhole-foundation/relayer-engine";
-import {CHAIN_ID_ACALA, CHAIN_ID_ETH, CHAIN_ID_MOONBEAM, CHAIN_ID_SOLANA, ChainId, TokenBridgePayload} from "@certusone/wormhole-sdk";
+import {
+  CHAIN_ID_ACALA,
+  CHAIN_ID_ETH,
+  CHAIN_ID_MOONBEAM,
+  CHAIN_ID_SOLANA,
+  ChainId,
+  TokenBridgePayload
+} from "@certusone/wormhole-sdk";
 import logger from "./logger";
 import {Contract, ethers} from "ethers";
 import {VersionedUserAction} from "./gmp";
@@ -25,7 +32,7 @@ const gmp = new Contract('0x0000000000000000000000000000000000000816', ['functio
       name: process.env.APP_NAME || `mrelayer11`,
       logger,
       spyEndpoint: process.env.SPY_ENDPOINT || "localhost:7073",
-      redis: { host: process.env.REDIS_HOST || "localhost", port: Number(process.env.REDIS_PORT) || 6379 },
+      redis: {host: process.env.REDIS_HOST || "localhost", port: Number(process.env.REDIS_PORT) || 6379},
       missedVaaOptions: {
         startingSequenceConfig: {
           [CHAIN_ID_ACALA as ChainId]: BigInt(process.env.ACA_FROM_SEQ || 3358),
@@ -38,39 +45,50 @@ const gmp = new Contract('0x0000000000000000000000000000000000000816', ['functio
 
   app.tokenBridge([CHAIN_ID_ACALA, CHAIN_ID_ETH, CHAIN_ID_SOLANA],
     async (ctx, next) => {
-      const { payload } = ctx.tokenBridge;
-      const { vaa, sourceTxHash } = ctx;
+      const {payload} = ctx.tokenBridge;
+      const {vaa, sourceTxHash} = ctx;
       const {payloadType, toChain, tokenTransferPayload} = payload;
       const to = payload.to.toString("hex");
       const logger = ctx.logger.child({sourceTxHash});
 
       if (payloadType === TokenBridgePayload.TransferWithPayload
-          && toChain === CHAIN_ID_MOONBEAM
-          && to === MRL_ADDRESS) {
+        && toChain === CHAIN_ID_MOONBEAM
+        && to === MRL_ADDRESS) {
 
         // TODO parse payload to filter only hydra ones
         //new VersionedUserAction('0x' + tokenTransferPayload.toString("hex"))
 
-        try {
-          logger.info(`Found VAA`);
-          await gmp.callStatic.wormholeTransferERC20(vaa.bytes, {nonce: currentNonce})
-          logger.info(`Completing transfer`);
-          const tx = await gmp.wormholeTransferERC20(vaa.bytes, {nonce: nextNonce()});
-          logger.info(`Transfer completed in ${tx.hash}`);
-          return next();
-        } catch (e) {
-          const text = JSON.stringify(e);
-          if (text.indexOf('transfer already completed') !== -1) {
-            logger.info(`Transfer already completed`);
+        async function completeTransfer(canRetry = true) {
+          try {
+            logger.info(`Found VAA`);
+            await gmp.callStatic.wormholeTransferERC20(vaa.bytes, {nonce: currentNonce})
+            logger.info(`Completing transfer`);
+            const tx = await gmp.wormholeTransferERC20(vaa.bytes, {nonce: currentNonce});
+            logger.info(`Transfer completed in ${tx.hash}`);
+            logger.info('Next nonce: ', nextNonce());
             return next();
+          } catch (e) {
+            const text = JSON.stringify(e);
+            if (text.indexOf('transfer already completed') !== -1) {
+              logger.info(`Transfer already completed`);
+              return next();
+            }
+            if (text.indexOf('Invalid GMP Payload') !== -1) {
+              logger.error(`Invalid GMP payload`);
+              return next();
+            }
+            if (canRetry && text.indexOf('nonce too low') !== -1) {
+              logger.info(`nonce too low, reloading nonce...`);
+              currentNonce = await moonbeam.getTransactionCount(signer.address);
+              await completeTransfer(false);
+            }
+            ctx.logger.error(e.error || e.message || e);
           }
-          if (text.indexOf('Invalid GMP Payload') !== -1) {
-            logger.info(`Invalid GMP payload`);
-            return next();
-          }
-          ctx.logger.error(e.error || e.message || e);
         }
+
+        await completeTransfer();
       }
+
     },
   );
 
--- src/logger.ts
+++ src/logger.ts
@@ -1,18 +1,21 @@
 import * as winston from "winston";
 
 export default winston.createLogger({
-    transports: [
-        new winston.transports.Console({
-            level: process.env.LOG_LEVEL || "info"
-        }),
-    ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.splat(),
-        winston.format.simple(),
-        winston.format.timestamp({
-            format: "YYYY-MM-DD HH:mm:ss.SSS",
-        }),
-        winston.format.errors({ stack: true }),
-    ),
+  transports: [
+    new winston.transports.Console({
+      level: process.env.LOG_LEVEL || "info"
+    }),
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.splat(),
+    winston.format.simple(),
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss.SSS",
+    }),
+    winston.format.errors({stack: true}),
+    winston.format.printf(({timestamp, level, message, stack}) => {
+      return `${timestamp} ${level}: ${stack || message}`;
+    })
+  ),
 });
